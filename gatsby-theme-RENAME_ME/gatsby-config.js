d9eaf8c8e327ee92618420867aaacdbd0d9cc74a
sw-yx <swyx@dontemail.me>
2019-07-27 16:36:31 -0400

    initial work with new gtdb v2

--- gatsby-theme-RENAME_ME/gatsby-config.js
+++
@@ -1,9 +0,0 @@
-// Take all options and pass them to gatsby-theme-blog
-module.exports = options => ({
-  plugins: [
-    {
-      resolve: `gatsby-theme-blog`,
-      options
-    }
-  ]
-});
---
+++ gatsby-theme-dev-blog/gatsby-node.js
@@ -0,0 +1,63 @@
+// // https://github.com/gatsbyjs/gatsby/issues/16142
+// exports.sourceNodes = ({ actions }) => {
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   console.log('hellloooo')
+//   const { createTypes } = actions
+//   createTypes(`
+//   type childImageSharp implements ImageSharp {
+//     foo: String
+//   }
+// `)
+//   //   createTypes(`
+//   // type ImageSharpFluid {
+//   //   base64: String
+//   //   tracedSVG: String
+//   //   aspectRatio: Float
+//   //   src: String
+//   //   srcSet: String
+//   //   srcWebp: String
+//   //   srcSetWebp: String
+//   //   sizes: String
+//   //   originalImg: String
+//   //   originalName: String
+//   //   presentationWidth: Int
+//   //   presentationHeight: Int
+//   // }
+//   // `)
+//   //   createTypes(`
+//   // type ImageSharpResolutions {
+//   //   base64: String
+//   //   tracedSVG: String
+//   //   aspectRatio: Float
+//   //   width: Float
+//   //   height: Float
+//   //   src: String
+//   //   srcSet: String
+//   //   srcWebp: String
+//   //   srcSetWebp: String
+//   //   originalName: String
+//   // }
+//   // `)
+//   //   createTypes(`
+//   // type ImageSharpSizes {
+//   //   base64: String
+//   //   tracedSVG: String
+//   //   aspectRatio: Float
+//   //   src: String
+//   //   srcSet: String
+//   //   srcWebp: String
+//   //   srcSetWebp: String
+//   //   sizes: String
+//   //   originalImg: String
+//   //   originalName: String
+//   //   presentationWidth: Int
+//   //   presentationHeight: Int
+//   // }
+//   // `)
+// }
